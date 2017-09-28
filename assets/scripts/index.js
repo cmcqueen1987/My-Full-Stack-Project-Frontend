2df6c6a4216063f84225355c88c091c7b489a597
cmcqueen1987 <charlie.mcqueen87@gmail.com>
2017-09-27 21:51:18 -0400

    made changes to css and html files
    countries in browser now easier for user to read and more
    display friendly

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -7,8 +7,6 @@ const countryEvents = require('./countryauth/events.js')
 
 $(() => {
   setAPIOrigin(location, config)
-})
-$(() => {
   authEvents.addHandlers()
   countryEvents.addHandlers()
 })
@@ -16,5 +14,8 @@ $(() => {
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 
+$(() => {
+
+})
+
 // use require without a reference to ensure a file is bundled
-// require('./example')
